e2d014c9b7f7f1e93b28e4a64986fb238226691e
Fernando J <fdo.jurado@gmail.com>
2026-02-24 16:12:28 -0500

    Add security field to CVModel and update YAML serialization methods

--- src/cv_schema/cv_model.py
+++ src/cv_schema/cv_model.py
@@ -9,6 +9,7 @@ from cv_schema.social import Social
 from cv_schema.research import Research
 from cv_schema.grant import Grant
 from cv_schema.affiliation import Affiliation
+from cv_schema.security import Security
 from cv_schema.yaml_serialize import YamlSerializable
 
 
@@ -23,29 +24,45 @@ class CVModel(YamlSerializable):
     publications: list[Publication]
     grants: list[Grant]
     affiliations: list[Affiliation]
-    long_intro: str = ""
-    short_intro: str = ""
+    long_intro: str
+    short_intro: str
     security: Security
 
     @classmethod
     def from_yaml(cls, yaml_data: dict):
         return cls(
             personal=PersonalInfo.from_yaml(yaml_data.get("personal", {})),
-            education=[Education.from_yaml(ed)
-                       for ed in yaml_data.get("education", [])],
+            social=Social.from_yaml(yaml_data.get("social", {})),
+            education=[Education.from_yaml(edu)
+                       for edu in yaml_data.get("education", [])],
+            research=Research.from_yaml(yaml_data.get("research", {})),
             experience=[Experience.from_yaml(
                 exp) for exp in yaml_data.get("experience", [])],
             google_scholar_author=GoogleScholarAuthor.from_yaml(
                 yaml_data.get("google_scholar_author", {})),
             publications=[Publication.from_yaml(
-                pub) for pub in yaml_data.get("publications", [])]
+                pub) for pub in yaml_data.get("publications", [])],
+            grants=[Grant.from_yaml(grant)
+                    for grant in yaml_data.get("grants", [])],
+            affiliations=[Affiliation.from_yaml(
+                aff) for aff in yaml_data.get("affiliations", [])],
+            long_intro=yaml_data.get("long_intro", ""),
+            short_intro=yaml_data.get("short_intro", ""),
+            security=Security.from_yaml(yaml_data.get("security", {}))
         )
 
     def to_yaml(self) -> dict:
         return {
             "personal": self.personal.to_yaml(),
-            "education": [ed.to_yaml() for ed in self.education],
+            "social": self.social.to_yaml(),
+            "education": [edu.to_yaml() for edu in self.education],
+            "research": self.research.to_yaml(),
             "experience": [exp.to_yaml() for exp in self.experience],
             "google_scholar_author": self.google_scholar_author.to_yaml(),
-            "publications": [pub.to_yaml() for pub in self.publications]
+            "publications": [pub.to_yaml() for pub in self.publications],
+            "grants": [grant.to_yaml() for grant in self.grants],
+            "affiliations": [aff.to_yaml() for aff in self.affiliations],
+            "long_intro": self.long_intro,
+            "short_intro": self.short_intro,
+            "security": self.security.to_yaml()
         }
