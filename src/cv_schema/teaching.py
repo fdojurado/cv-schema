5416a056da78fd385c48528550c02a582b2488f0
Fernando J <fdo.jurado@gmail.com>
2026-02-24 21:58:37 -0500

    Add methods for YAML serialization in Teaching class

--- src/cv_schema/teaching.py
+++ src/cv_schema/teaching.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 from cv_schema.yaml_serialize import YamlSerializable
 
+
 @dataclass
 class TeachingDate(YamlSerializable):
     start_date: str
@@ -31,4 +32,29 @@ class Teaching(YamlSerializable):
     dates: list[TeachingDate]
     type: str
     responsibilities: list[str]
-    
+
+    @classmethod
+    def from_yaml(cls, yaml_data: dict):
+        return cls(
+            id=yaml_data.get("id", ""),
+            subject=yaml_data.get("subject", ""),
+            institution_id=yaml_data.get("institution_id", 0),
+            role=yaml_data.get("role", ""),
+            url=yaml_data.get("url", ""),
+            dates=[TeachingDate.from_yaml(date)
+                   for date in yaml_data.get("dates", [])],
+            type=yaml_data.get("type", ""),
+            responsibilities=yaml_data.get("responsibilities", [])
+        )
+
+    def to_yaml(self) -> dict:
+        return {
+            "id": self.id,
+            "subject": self.subject,
+            "institution_id": self.institution_id,
+            "role": self.role,
+            "url": self.url,
+            "dates": [date.to_yaml() for date in self.dates],
+            "type": self.type,
+            "responsibilities": self.responsibilities
+        }
